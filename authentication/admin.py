e3b599ce7220e0811c2662f4a24497ddec72d009
calvincetom <calvincetom@outlook.com>
2025-07-18 18:12:15 +0300

    update model registration

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,19 @@
+# authentication/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_google_user', 'is_staff')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Google Auth', {'fields': ('google_id', 'is_google_user')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
